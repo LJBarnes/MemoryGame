768a144f0ec3a320ee5953edb316326b012d2bc6
Leah Barnes <johnson.leahe@gmail.com>
2020-05-03 21:38:53 -0400

    added shuffle function. added flip function.

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
+class MixOrMatch {
+    constructor(totalTime, cards){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById ('flips');
+
+    }
+    startGame(){
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+
+        this.shuffleCards();
+    }
+    flipCard(card){
+        if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
+
+            // if statement for match/nonmatch
+        }
+    }
+
+    shuffleCards(){
+        for(let i = this.cardsArray.length -1; i >0; i--) {
+            // math.random creates a random float between 0 and one, excluding one
+            let randIndex = Math.floor(Math.random()* (i+1));
+            this.cardsArray[randIndex].style.order = i;
+            this.cardsArray[i].style.order = randIndex;
+        }
+    }
+
+    canFlipCard(card){
+        return true;
+        // return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+    }
+}
+
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        })
+    })
+    cards.forEach(card => {
+      card.addEventListener('click', () => {
+          game.flipCard(card);
+      });  
+    });
+}
+
 if (document.readyState === "loading"){
     document.addEventListener('DOMContentLoaded', ready());
 } else {
     ready();
 }
 
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-        })
-    })
-}
+// new MixOrMatch(100,)
